34a7ba523a472c3085cfb61c20965d1eb29ebad8
Marina James <MarinaJames@Marinas-MacBook-Air.local>
2017-06-13 19:34:06 -0400

    entered questions into json files for both basic and cloze

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Flashcard-Generator
+
+## Overview
+A basic flashcard application which has both Basic flashcards and Cloze flashcards.
+Basic flashcards have both a front and back. 
--- assets/javascript/ClozeCard.js
+++ assets/javascript/ClozeCard.js
@@ -1,7 +1,45 @@
-module.exports = ClozeCard;
+var cardData = require('../../clozeCard.json');
+console.log(cardData);
 
-function ClozeCard(fullText, partial, cloze) {
-	this.text = fullText;
-	this.partial = partial;
+function ClozeCard(text, cloze) {
+	this.text = text;
 	this.cloze = cloze;
 }
+
+module.exports = ClozeCard;
+
+var questionOneCloze = new ClozeCard('The Red Wings are the Detroit NHL team', 'The Red Wings');
+//'The Red Wings'
+console.log(questionOneCloze.cloze);
+// '... are the Detroit NHL team'
+console.log(questionOneCloze.partial);
+//'The Red Wings are the Detroit NHL team'
+console.log(questionOneCloze.fullText)
+
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
+
+var questionTwoCloze = new ClozeCard('The Blackhawks are the Chicago NHL team?', 'The Blackhawks');
+// 'What is the name of the Chicago hockey team?'
+console.log(questionTwoCloze.front);
+// 'Blackhawks'
+console.log(questionTwoCloze.back);
+
+var questionThreeCloze = new ClozeCard('The Avalanche are the Colorado NHL team?', 'The Avalanche');
+// 'What is the name of the Colorado hockey team?'
+console.log(questionThreeCloze.front);
+// 'Avalanche'
+console.log(questionThreeCloze.back);
+
+var questionFourCloze = new ClozeCard('The Blues are the St.Louis NHL team?', 'The Blues');
+// 'What is the name of the St.Louis hockey team?'
+console.log(questionFourCloze.front);
+// 'Blues'
+console.log(questionFourCloze.back);
+
+var questionFiveCloze = new ClozeCard('The Sharks are the San Jose NHL team?', 'The Sharks');
+// 'What is the name of the San Jose hockey team?'
+console.log(questionFiveCloze.front);
+// 'Sharks'
+console.log(questionFiveCloze.back);
